ba3d37eee6f4ad77038d3bed2579542416e06c62
manish <maanishkaale@gmail.com>
2021-10-17 11:44:22 +0530

    implemented where the empty string returns zero

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -9,7 +9,7 @@ class StringCalculatorShould {
     @Test
     void empty_string_should_return_0() {
         StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
+        assertEquals(stringCalculator.calculate(""), 0);
     }
 
     @Test
